706f1058ef8140e99f5180a46f7c4ce86314d9dc
Erick Ventura <erick94111@gmail.com>
2024-07-09 02:21:17 -0600

    chore: add missing decoratos on dtos

--- src/ses-parser/dtos/input/mail/common-headers.dto.ts
+++ src/ses-parser/dtos/input/mail/common-headers.dto.ts
@@ -1,8 +1,23 @@
+import { IsArray, IsEmail, IsString } from 'class-validator';
+
 export class CommonHeadersDto {
+  @IsString()
   returnPath: string;
+
+  @IsArray()
+  @IsEmail(undefined, { each: true })
   from: string[];
+
+  @IsString()
   date: string;
-  to: string;
+
+  @IsArray()
+  @IsEmail(undefined, { each: true })
+  to: string[];
+
+  @IsString()
   messageId: string;
+
+  @IsString()
   subject: string;
 }
--- src/ses-parser/dtos/input/mail/header.dto.ts
+++ src/ses-parser/dtos/input/mail/header.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class HeaderDto {
+  @IsString()
   name: string;
+
+  @IsString()
   value: string;
 }
