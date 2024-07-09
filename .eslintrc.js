4a55e18493eb7e5c97ef8513caede114aeb235a8
Erick Ventura <erick94111@gmail.com>
2024-07-08 23:54:20 -0600

    chore: add eslint, prettier and husky configs, clean up project

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'jest', 'import'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -15,11 +17,35 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'jest/prefer-expect-assertions': 'warn',
+    'no-console': 'error',
+    'max-len': ['error', { code: 150, tabWidth: 2, ignoreUrls: true, ignoreComments: true }],
+    semi: 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    curly: ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
   },
 };
